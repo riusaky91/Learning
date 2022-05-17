a1d673019bb0434c1ad95212530cccb14193e238
SDS/mlestupinan <mlestupinan@saludcapital.gov.co>
2022-05-17 15:38:37 -0500

    refinamiento de metodo 24

--- JavaScript/Ejercicios_Basicos/js/ejercicio1.js
+++ JavaScript/Ejercicios_Basicos/js/ejercicio1.js
@@ -342,10 +342,12 @@ el primero tendrá los numeros ordenados en forma ascendente y el segundo de for
  pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
 */
 
-let arr = [ 7, 5,7,8,6 ];
-console.log(arr.sort((a,b)=>b-a));
-console.log(arr.sort((a,b)=>a-b));
+const orden = (arreglo = undefined) =>{
 
+    return console.log(`asc: [${arreglo.sort((a,b)=>a-b)}], desc: [${arreglo.sort((a,b)=>b-a)}]`);
+}
+
+//orden([7, 5,7,8,6]);
 
 /*
 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, 
