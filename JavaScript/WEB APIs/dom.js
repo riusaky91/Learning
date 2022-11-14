74deec1bccb4f4c205ba603bbc576c41ab5cfd18
riusaky91 <marlonleonardo.er@gmail.com>
2022-11-14 13:38:02 -0500

    Introduccion al DOM

---
+++ JavaScript/WEB APIs/dom.js
@@ -0,0 +1,29 @@
+
+/*console.log(window);
+console.log(document);
+
+let texto = "Bienvenido Marlon al mundo de las APIS"
+
+const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto));//Utlizando API del navegador para hablar
+
+hablar(texto);*/
+
+
+console.log("***************************************************************************************************");
+console.log("ELEMENTOS DEL DOCUMENTO");//DOM
+console.log(" ");
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+console.log(document.getSelection().toString());
+document.write("<h2>Hola Mundo desde el DOM</h2>")
