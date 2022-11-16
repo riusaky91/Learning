9747de722f9a067f4a543f1719f15b7640128f7c
riusaky91 <marlonleonardo.er@gmail.com>
2022-11-15 20:02:53 -0500

    Nodos elementos y selectores

--- JavaScript/WEB APIs/dom.js
+++ JavaScript/WEB APIs/dom.js
@@ -10,7 +10,7 @@ hablar(texto);*/
 
 
 console.log("***************************************************************************************************");
-console.log("ELEMENTOS DEL DOCUMENTO");//DOM
+console.log("ELEMENTOS DEL DOCUMENTO");//DOM api de Javascript para interpretar documentos html y xml
 console.log(" ");
 
 console.log(document);
@@ -27,3 +27,20 @@ console.log(document.styleSheets);
 console.log(document.scripts);
 console.log(document.getSelection().toString());
 document.write("<h2>Hola Mundo desde el DOM</h2>")
+
+
+console.log("***************************************************************************************************");
+console.log("Nodos, Elementos y Selectores");//DOM
+console.log(" ");
+
+console.log(document.getElementsByTagName("li"));//Trae todos los elmentos del html li --Ya no se utiliza
+console.log(document.getElementsByClassName("card"));//trae todos los elementos con clase card --Ya no se utiliza
+console.log(document.getElementsByName("nombre"));//trae todos los elementos con name nombre --Ya no se utiliza
+console.log(document.getElementById("menu"));//Trae un elemento con id menu //es mas optimo cuando hablamos de iDs utilizar este metodo
+console.log(document.querySelector("#menu"));//Trae el primer selector como nodo del tipo que se quiera # =para ids . para clases
+console.log(document.querySelectorAll("a"));// Devuelve nodos no arreglos
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach(element => console.log(element));//recorriendo nodos de enlaces
+console.log(document.querySelector(".card"));
+console.log(document.querySelectorAll(".card")[2]);//comportamiento de arreglo
+console.log(document.querySelectorAll("#menu li"));//Dentro del id menu tarer todas las etiquetas li
