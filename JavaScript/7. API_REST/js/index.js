ad8382a950179de0a0fd3b90e532bdb85e5e4d94
riusaky91 <marlonleonardo.er@gmail.com>
2023-06-11 19:54:29 -0500

    APIREST XHR -2

--- JavaScript/7. API_REST/js/index.js
+++ JavaScript/7. API_REST/js/index.js
@@ -1,8 +1,40 @@
 import { xhr } from "./xhr.js"; //manejo de modulos, exportacion de metodos para la interfaz al no ser por defecto se deben usar los corchetes
 
-const d = document;
+const d= document,
+    $table = d.querySelector(".crud-table"), 
+    $form = d.querySelector(".crud-form"), 
+    $title = d.querySelector(".crud-title"), 
+    $template = d.getElementById("crud-template").content,
+    $fragment = d.createDocumentFragment(); //declaro e inicializo la constante que contiene un fragmento
 
 
-d.addEventListener("DOMContentLoaded",(e)=>{
-    xhr();//CRUD con el objeto XmlHttpRequest
-});
+const getAll = ()=>{
+    xhr({
+        method: "GET",
+        url: "http://localhost:3000/santos",
+        success: (res)=>{console.log(res);
+            res.forEach(element => {//recorro el obeto javascript generado
+                
+                
+                $template.querySelector(".name").textContent = element.nombre;
+                $template.querySelector(".constellation").textContent = element.constelacion;
+                $template.querySelector(".edit").dataset.id = element.id;//Creo un data atribute para el elemento editar con el valor del id
+                $template.querySelector(".edit").dataset.name = element.nombre;//Creo un data atribute para el elemento editar con el valor del nombre
+                $template.querySelector(".edit").dataset.contellation = element.constelacion;//Creo un data atribute para el elemento editar con el valor de la constelacion
+                $template.querySelector(".delete").dataset.id = element.id;//Creo un data atribute para el elemento eliminar con el valor del id
+
+                let $clone = d.importNode($template, true);//Clonando toda la estructura interna previamente creada en el template ya que solo puede haber un template
+                $fragment.appendChild($clone);//agrego el clon creado a el fragmento para no injectar en todo el dom
+                
+            });
+            $table.querySelector("tbody").appendChild($fragment);//agrego al elemento del dom el fragmeto con los elementos li
+
+        },
+        error: (err)=>{console.error(err);
+            $table.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`)
+        },
+        data: null
+    })
+}
+
+d.addEventListener("DOMContentLoaded",getAll);//CRUD con el objeto XmlHttpRequest
--- JavaScript/7. API_REST/js/xhr.js
+++ JavaScript/7. API_REST/js/xhr.js
@@ -6,12 +6,10 @@ const d= document,
     $template = d.getElementById("crud-template").content,
     $fragment = d.createDocumentFragment(); //declaro e inicializo la constante que contiene un fragmento
 
-export function xhr (){
-
 //Funcion API_REST con el Objeto XMLHttpRequest
+export function xhr (options){
 
-
-    
+    let {url,method, success, error, data } = options;//destruccturacion del objeto en las variables inicializadas
     
     const xhr = new XMLHttpRequest();//instancia del objeto para hacer un request
 
@@ -29,23 +27,13 @@ export function xhr (){
 
             let json = JSON.parse(xhr.responseText);//ddclaro e inicializo la variable json con la respuesta tipo JSON formateada como objeto javascript
             
-
-            json.forEach(element => {//recorro el obeto javascript generado
-                
-                $template.querySelector(".constellation").setAttribute("id", element.id);
-                $template.querySelector(".name").textContent = element.nombre;
-                $template.querySelector(".constellation").textContent = element.constelacion;
-                let $clone =document.importNode($template, true);//Clonando toda la estructura interna previamente creada en el template ya que solo puede haber un template
-                $fragment.appendChild($clone);//agrego el clon creado a el fragmento para no injectar en todo el dom
-                
-            });
+            success(json);
             
 
-            $table.querySelector("tbody").appendChild($fragment);//agrego al elemento del dom el fragmeto con los elementos li
         }else{//en caso de que la respuesta no se satisfactoria
             
             let message = xhr.statusText || "Ocurrio un error";//operador corto circuito que toma el segundo valor si el primero esta vacio
-            $xhr.innerHTML = `Error ${xhr.status}: ${message}`;//Imprimo y añado el error encontrado al elemento del dom
+            error(`Error ${xhr.status}: ${message}`);//Imprimo y añado el error encontrado al elemento del dom
         }
 
        //console.log("Este mensaje cargara de cualquier forma");//mensaje que se ejecuta independiente de la respuesta del servidor
@@ -54,18 +42,12 @@ export function xhr (){
 
 
 
-    xhr.open("GET","http://localhost:3000/santos");//abrir la peticion (metodo y end point(ruta local o web))
+    xhr.open(method || "GET",url);//abrir la peticion (metodo con corto circuito y end point(ruta local o web))
         
-    xhr.send();//Envio sin datos para consumir
-
-    d.addEventListener("click",(e)=>{
-        if(e.target.matches(".delete")){
-            console.log(e.target.parentElement.previousElementSibling.id);
-            xhr.open("DELETE",`http://localhost:3000/santos/${e.target.parentElement.previousElementSibling.id}`);//abrir la peticion (metodo y end point(ruta local o web))
-        
-            xhr.send();//Envio sin datos para consumir
-        }
-
-    })
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");//header de la solicitud
+    xhr.send(JSON.stringify(data));//Envio sin datos para consumir con viero json a texto
 
 }
+
+
+
