ec9efaa47f433f334985002ecd695ba44663a0fd
riusaky91 <marlonleonardo.er@gmail.com>
2025-08-24 21:12:49 -0500

    implementacion de variables de entorno y router links

--- ANGULAR/gifs-app/src/app/gifs/components/gif-side-menu/gif-side-menu-header/gif-side-menu-header.ts
+++ ANGULAR/gifs-app/src/app/gifs/components/gif-side-menu/gif-side-menu-header/gif-side-menu-header.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { environment } from "@environments/environment"; // Importo las variables de entorno uso de alias para no usar rutas relativas
+
 
 @Component({
   selector: "gif-side-menu-header",
@@ -7,5 +9,5 @@ import { Component } from "@angular/core";
 })
 
 export default class GifSideMenuHeaderComponent { //Default para exportar el componente y usar lazy loading
-  
+  envs = environment;// Traigo las variables de entorno para usarlas en el html
 }
--- ANGULAR/gifs-app/src/app/gifs/components/gif-side-menu/gif-side-menu-options/gif-side-menu-options.ts
+++ ANGULAR/gifs-app/src/app/gifs/components/gif-side-menu/gif-side-menu-options/gif-side-menu-options.ts
@@ -1,11 +1,33 @@
 import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string;  // Etiqueta que se mostrará en la opción del menú
+  subLabel: string; // Subtítulo o descripción adicional de la opción
+  route: string;  // Ruta a la que navegará al seleccionar la opción
+  icon: string;  // Icono asociado a la opción del menú
+}
 
 @Component({
   selector: "gif-side-menu-options",
   templateUrl: "./gif-side-menu-options.html",
+  imports: [RouterLink, RouterLinkActive],
   
 })
 
 export default class GifSideMenuOptionsComponent { //Default para exportar el componente y usar lazy loading
-  
+  menuOptions: MenuOption[] =  [
+      {
+        label: "Trending",
+        subLabel: "Gifs más populares",
+        route: "/Dashboard/Trending",
+        icon: "fa-solid fa-chart-line"
+      },
+      {
+        label: "Search",
+        subLabel: "Buscar gifs",
+        route: "/Dashboard/Search",
+        icon: "fa-solid fa-magnifying-glass"
+      }
+    ]
 }
