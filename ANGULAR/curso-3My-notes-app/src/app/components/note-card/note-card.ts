4a9096520a56494a593abef910a544f7f906e0a6
Marlon Leonardo <MLEstupinan@saludcapital.gov.co>
2025-10-07 16:30:30 -0500

    inicio input y cards terminadas

--- ANGULAR/curso-3My-notes-app/src/app/components/note-card/note-card.ts
+++ ANGULAR/curso-3My-notes-app/src/app/components/note-card/note-card.ts
@@ -1,14 +1,19 @@
 import { Component, input } from '@angular/core';
 import Note from '../../../models/Note';
 import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-note-card',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './note-card.html',
   styleUrl: './note-card.css'
 })
 export class NoteCard {
-  note = input<Note>();
-
+  note = input<Note>(); // nota que recibe el componente
+  value : string = 'Clear me'; // valor inicial del input
 }
