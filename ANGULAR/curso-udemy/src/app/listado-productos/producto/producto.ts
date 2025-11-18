b201885ff173b534fde045d0be6f210304ddfb72
Marlon Leonardo <MLEstupinan@saludcapital.gov.co>
2025-11-18 16:29:53 -0500

    reto input output

--- ANGULAR/curso-udemy/src/app/listado-productos/producto/producto.ts
+++ ANGULAR/curso-udemy/src/app/listado-productos/producto/producto.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductoModule } from '../../producto/producto-module';
 
 @Component({
   selector: 'app-producto',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './producto.css'
 })
 export class Producto {
-  descripcion: string = 'Nuevo Producto'; // Título del producto
+
+  @Input() producto!: ProductoModule; // Propiedad para recibir el mensaje del componente padre si esta vacia no muestra nada 
+
   precio: number = 99.99; // Precio del producto
 }
