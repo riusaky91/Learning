20739e4f60da6f47044c32eacfc59694030213b3
Marlon Leonardo <MLEstupinan@saludcapital.gov.co>
2026-08-18 08:19:08 -0500

    control-clientes / funcionamiento sin conexion persistencialocal del navegador

--- ANGULAR/control-clientes/src/main.ts
+++ ANGULAR/control-clientes/src/main.ts
@@ -4,19 +4,36 @@ import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from './environments/environment.development';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideStorage, getStorage } from '@angular/fire/storage';
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, {
   ...appConfig, // Desestructuramos el appConfig para pasar sus propiedades al bootstrap
   providers: [ 
     provideHttpClient(), 
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // Inicializa Firebase 
-    provideFirestore(() => getFirestore()), // Inicializa Firestore 
+
+    // Configuración moderna de Firestore con Persistencia Offline
+    provideFirestore(() => {
+      const app = initializeApp(environment.firebaseConfig);
+      return initializeFirestore(app, {
+        localCache: persistentLocalCache({
+          tabManager: persistentMultipleTabManager() // Permite sincronizar si el usuario abre varias pestañas
+        })
+      });
+    }), 
     provideAuth(() => getAuth()), // Inicializa Auth 
     provideStorage(() => getStorage()), // Inicializa Storage 
-    ...appConfig.providers // Desestructuramos los providers del appConfig si es necesario
+    
+    ...appConfig.providers, 
+    
+    provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }) // Desestructuramos los providers del appConfig si es necesario
   ]
 
 })
